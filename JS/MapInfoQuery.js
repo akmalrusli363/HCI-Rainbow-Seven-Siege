b11554d158ad821e6d0c8908e74b62b5da65ced8
akmalrusli363 <akmalrusli363@outlook.com>
2020-06-02 23:47:10 +0700

    Finished map detail & query using JSON

--- JS/MapInfoQuery.js
+++ JS/MapInfoQuery.js
@@ -13,7 +13,7 @@ var jsonMapInfo = {
             "imageDir": "map-border.jpg",
             "description": "Team Rainbow has been called in to assist the local law enforcement and regain control of the border. A mix of old and new buildings with open air pathways make this the most destructible and open planned map to date.",
             "location": "Middle East",
-            "gamemode": ["Quick Match", "Ranked", "Quarantine"]
+            "gamemode": ["Quick Match", "Ranked"]
         },
         {
             "mapID": "clubhouse",
@@ -30,6 +30,134 @@ var jsonMapInfo = {
             "description": "Team Rainbow has been called to safeguard the seashore of Ibiza, Spain. Stationed on Ibiza’s rocky shore, course through Balearic ruins and navigate booming bars to neutralize the threat. Get ready for a surround flow of combat laced with chaotic rushes as you explore a paradise gone wrong.",
             "location": "Ibiza, Spain",
             "gamemode": ["Quick Match", "Ranked", "Quarantine"]
+        },
+        {
+            "mapID": "consulate",
+            "mapName": "Consulate",
+            "imageDir": "map-consulate.jpg",
+            "description": "A high-risk, highly secured fortified location, this map depicts an assault on a French consulate in Ivory Coast.",
+            "location": "Abidjan, Ivory Coast",
+            "gamemode": ["Quick Match", "Ranked"]
+        },
+        {
+            "mapID": "favela",
+            "mapName": "Favela",
+            "imageDir": "map-favela.jpg",
+            "description": "Team Rainbow has been called to pacify and regain control of the Favela. Mixing colorful and narrow streets with high vantage points, this map supports exterior shell destruction which renders it as the most destructible to date.",
+            "location": "Rio de Janerio, Brazil",
+            "gamemode": ["Quick Match", "Ranked", "Quarantine"]
+        },
+        {
+            "mapID": "fortress",
+            "mapName": "Fortress",
+            "imageDir": "map-fortress.jpg",
+            "description": "This large daytime map features a mudbrick kasbah and iconic architecture unique to the southern region of Morocco. The imposing main entrance greets all visitors with its two towers and Kasbah Sehkra Mania written above the archway.",
+            "location": "Morocco",
+            "gamemode": ["Quick Match", "Ranked"]
+        },
+        {
+            "mapID": "hereford",
+            "mapName": "Hereford Base",
+            "imageDir": "map-hereford.jpg",
+            "description": "This is where SAS agents learn their craft and as such it a patchwork of plywood targets, sandbags, barbed wire and other obstacles. One of the most archetypal examples of siege gameplay.",
+            "location": "Herefordshire, United Kingdom",
+            "gamemode": ["Quick Match", "Quarantine"]
+        },
+        {
+            "mapID": "house",
+            "mapName": "House",
+            "imageDir": "map-house.jpg",
+            "description": "Playing on themes of familiarity and vulnerability, this encounter takes place in a typical suburban home. It’s a prime example of destructive depth and replayability, and is one of the showcases for classic siege gameplay.",
+            "location": "Los Angeles, California, USA",
+            "gamemode": ["Quick Match", "Quarantine"]
+        },
+        {
+            "mapID": "kafe",
+            "mapName": "Kafe Dostoyevsky",
+            "imageDir": "map-kafe.jpg",
+            "description": "This high-class café in central Moscow is the pinnacle of glitz and glamour, and is a natural intervention ground for Spetsnaz forces. Destruction of the rich, chic, classic decor will be a key element of this map.",
+            "location": "Moscow, Russia",
+            "gamemode": ["Quick Match", "Ranked"]
+        },
+        {
+            "mapID": "kanal",
+            "mapName": "Kanal",
+            "imageDir": "map-kanal.jpg",
+            "description": "Taking place in the industrial area of Hamburg, Germany, ‘Kanal’ involves multiple buildings with strategic connecting points over canals. It features many interesting strategic possibilities created by the many bridges, buildings and related vantage points of its terrain.",
+            "location": "Hamburg, Germany",
+            "gamemode": ["Ranked"]
+        },
+        {
+            "mapID": "oregon",
+            "mapName": "Oregon",
+            "imageDir": "map-oregon.jpg",
+            "description": "Oregon depicts an assault on a fortified survivalist compound. It feels rustic, eclectic and lived-in, and covers a vast, spread out layout requiring adapted tactics to sweep and hold effectively.",
+            "location": "Redmond, Oregon, USA",
+            "gamemode": ["Quick Match", "Ranked", "Quarantine"]
+        },
+        {
+            "mapID": "outback",
+            "mapName": "Outback",
+            "imageDir": "map-outback.jpg",
+            "description": "Set in Australia’s Red Heart, this map is an homage to all the dusty service stations and motels that populate the forgotten highways of the continent, places filled with hardworking locals and specialty menu items you’ll find nowhere else.",
+            "location": "Australian Outback",
+            "gamemode": ["Quick Match", "Ranked", "Quarantine"]
+        },
+        {
+            "mapID": "plane",
+            "mapName": "Presidental Plane",
+            "imageDir": "map-plane.jpg",
+            "description": "This presidential plane offers an exotic and unique siege experience. It is claustrophobic, tense and constrained, involving limited breaching opportunities and sight lines.",
+            "location": "Heathrow Airport, West London, United Kingdom",
+            "gamemode": ["Quick Match", "Quarantine"]
+        },
+        {
+            "mapID": "skyscraper",
+            "mapName": "Skyscraper",
+            "imageDir": "map-skyscraper.jpg",
+            "description": "Team Rainbow has been called to neutralize a Yakuza war brewing high above the streets of Nagoya, Japan. Perched on top of Skyscraper, you will explore refined works of Japanese architecture inside a dangerous mansion. Be prepared for vertiginous rappels in the haze of the morning sun, where modernity and tradition meet.",
+            "location": "Nagoya, Japan",
+            "gamemode": ["Quick Match", "Ranked"]
+        },
+        {
+            "mapID": "stadium",
+            "mapName": "Stadium",
+            "imageDir": "map-stadium.jpg",
+            "description": "The competition takes place in the Stadium. The new map uses bulletproof glass material to create new situations. In the Stadium, you have access to all Operators.",
+            "location": "Elis, Greece",
+            "gamemode": ["Quick Match"]
+        },
+        {
+            "mapID": "themepark",
+            "mapName": "Theme Park",
+            "imageDir": "map-themepark.jpg",
+            "description": "Team Rainbow has been called to raid an abandoned theme park on the shores of Hong Kong. Lead a lethal raid in this challenging perimeter. Get ready to discover a colorful and eclectic new map filled with dynamic and fast-paced face-offs.",
+            "location": "Kowloon, Hong Kong",
+            "gamemode": ["Quick Match", "Ranked", "Quarantine"]
+        },
+        {
+            "mapID": "tower",
+            "mapName": "Tower",
+            "imageDir": "map-tower.jpg",
+            "description": "Scale the snow-capped Mok Myeok Tower to lead a rescue mission in Seoul, South Korea. The modern communications and observation tower offers fresh vertical vantage points overlooking the entire cityscape, and sets the stage for diverse multilevel tactics.",
+            "location": "Seoul, South Korea",
+            "gamemode": ["Quick Match"]
+        },
+        {
+            "mapID": "villa",
+            "mapName": "Villa",
+            "imageDir": "map-villa.jpg",
+            "description": "The land belongs to the Vinciguerra, a dangerous crime family known for their smuggling activities. Now we catch the villa in a snapshot of time: Vinciguerras are burning documents and transporting their holdings before the big raid. But they’ve got wind of Operation Para Bellum and fled, leaving everything in disarray with vital information ready for the taking.",
+            "location": "Tuscany, Italy",
+            "gamemode": ["Quick Match", "Ranked"]
+        },
+        {
+            "mapID": "yacht",
+            "mapName": "Yacht",
+            "imageDir": "map-yacht.jpg",
+            "description": "In the Baffin Bay, a luxury yacht has crashed into an iceberg breaching the hull. Rainbow has been sent to secure the stranded vessel as an unidentified submarine has docked near the vessel.",
+            "location": "Baffin Bay",
+            "gamemode": ["Quick Match"]
         }
     ]
 };
