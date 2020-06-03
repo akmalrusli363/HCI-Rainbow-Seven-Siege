e4b826dc99d9c5fd70620f508d23da9b5a807eb5
akmalrusli363 <akmalrusli363@outlook.com>
2020-06-03 20:50:42 +0700

    Fix and adjust PreOrder CSS element & positioning

--- JS/Pre-OrderScript.js
+++ JS/Pre-OrderScript.js
@@ -1,59 +1,40 @@
-
-function formValidation(){
-
-    
-    
-    var username  = new String(document.forms["form1"]["username"].value);
+function formValidation() {
+    var username = new String(document.forms["form1"]["username"].value);
     var password = new String(document.forms["form1"]["password"].value);
     var address = new String(document.forms["form1"]["address"].value);
     var email = new String(document.forms["form1"]["email"].value);
-    
+
     //validate username
-
-
-   
-
-    if(username.length < 1){
+    if (username.length < 1) {
         window.alert("Username must be filled!");
         return false;
-    }
-    else if(username.length < 5){
+    } else if (username.length < 5) {
         window.alert("Username must contain at least 5 characters!");
         return false;
     }
 
     //validate email
-
-    else if(email.length < 1){
+    else if (email.length < 1) {
         window.alert("Email must be filled!");
         return false;
     }
 
     //validate password
-
-    else if(password.length < 1){
+    else if (password.length < 1) {
         window.alert("Password must be filled!");
         return false;
-    }
-
-    else if(password.length < 6){
+    } else if (password.length < 6) {
         window.alert("Password must contain at least 6 characters!");
         return false;
     }
 
     //validate address
-
-    else if(address.length < 10){
+    else if (address.length < 10) {
         window.alert("Address must contain at least 10 characters!");
         return false;
     }
-   
-
-    
 
     window.alert("Your order has been placed");
 
-    return true
+    return true;
 }
-
-
