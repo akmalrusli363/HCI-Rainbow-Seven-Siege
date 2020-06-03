09993b9d487f606af27265af4624e525683ba21b
Akmal Wiraprana <akmalwiraprana26@gmail.com>
2020-06-03 19:31:23 +0700

    delete Pre-Order.css and fix Pre-order style(not done

--- JS/Pre-OrderScript.js
+++ JS/Pre-OrderScript.js
@@ -14,44 +14,44 @@ function formValidation(){
    
 
     if(username.length < 1){
-        window.alert("username must be filled");
+        window.alert("Username must be filled!");
         return false;
     }
     else if(username.length < 5){
-        window.alert("username must contain at least 5 characters");
+        window.alert("Username must contain at least 5 characters!");
         return false;
     }
 
     //validate email
 
     else if(email.length < 1){
-        window.alert("email must be filled");
+        window.alert("Email must be filled!");
         return false;
     }
 
     //validate password
 
     else if(password.length < 1){
-        window.alert("password must be filled");
+        window.alert("Password must be filled!");
         return false;
     }
 
     else if(password.length < 6){
-        window.alert("password must contain at least 6 characters");
+        window.alert("Password must contain at least 6 characters!");
         return false;
     }
 
     //validate address
 
     else if(address.length < 10){
-        window.alert("address must contain at least 10 characters");
+        window.alert("Address must contain at least 10 characters!");
         return false;
     }
    
 
     
 
-    window.alert("your order has been placed");
+    window.alert("Your order has been placed");
 
     return true
 }
